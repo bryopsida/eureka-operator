7c27ce45c62846488cb09fdd187414539a43e228
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-04-11 17:36:01 -0500

    wip: comms

--- node/src/comms.mjs
+++ node/src/comms.mjs
@@ -1,10 +1,15 @@
 import { Eureka } from '@bryopsida/eureka'
+import {EventEmitter} from 'node:events'
 
-export class EurekaOperatorComms {
+export class EurekaOperatorComms extends EventEmitter {
   constructor (opts) {
     if (!opts.beaconInterval) throw new Error('opts.beaconInterval is required!')
     if (!opts.id) throw new Error('opts.id is required!')
     if (!opts.password && !opts.passwordProvider) throw new Error('opts.password or opts.passwordProvider are required!')
     if (!opts.salt && !opts.saltProvider) throw new Error('opts.salt or opts.saltProvider are required!')
+    super()
+    this._eureka = new Eureka({
+      
+    })
   }
 }
